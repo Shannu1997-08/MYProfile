2e76de523a7c304be449f46d5a8f39ca5fb35223
Shannu1997-08 <shanmukhvemala@gmail.com>
2025-10-08 19:04:11 +0530

    chnges

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // or BrowserRouter
+import { HashRouter } from "react-router-dom"; // Use HashRouter
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <App />
